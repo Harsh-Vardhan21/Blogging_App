68cee60e6aca9cddb3abf7e6e1b943d033d6f3cb
Harsh Vardhan <harsh.1vardhan123@gmail.com>
2024-01-28 21:28:48 +0530

    creates api models

--- api/index.js
+++ api/index.js
@@ -1,6 +1,8 @@
 import express from 'express';
 import mongoose from 'mongoose';
 import dotenv from 'dotenv';
+import userRoutes from './routes/user.route.js';
+import authRoutes from './routes/auth.route.js'
 
 dotenv.config();
 mongoose
@@ -18,6 +20,5 @@ app.listen(3000 , ()=>{
     console.log('Server is running on port 3000');
 })
 
-app.get('/test' , (req , res) => {
-    res.json({message : 'API is working.'});
-})
+app.use('/api/user',userRoutes)
+app.use('./api/auth',authRoutes)
